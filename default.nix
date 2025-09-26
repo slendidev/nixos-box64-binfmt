0d4228f0e0c8a7a04d53ee8ff9aa9f08f026734b
Slendi <slendi@socopon.com>
2025-09-27 01:15:31 +0300

    adsf
    Signed-off-by: Slendi <slendi@socopon.com>

--- default.nix
+++ default.nix
@@ -620,7 +620,7 @@ let
     owner = "ptitSeb";
     repo = "box64";
     rev = "main";
-    sha256 = "sha256-55kNxQ/aDIRFFp7qFrevQpAKHXa6VWbSSfyZGbm1kJ0=";
+    sha256 = "sha256-/2USXRtsmhnsR0DNPmidNQ4JV6Gs8k5W5RIIPu6u4V4=";
   };
 in
 
